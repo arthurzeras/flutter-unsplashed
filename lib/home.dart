c80bd66e5f1fb1892e0fe0822e80f3d7a4eeb81c
arthurzeras <arthurolvmorais@gmail.com>
2019-10-08 15:49:30 -0300

    Pass home content to other file

---
+++ lib/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenWidget extends StatefulWidget {
+  HomeScreenWidget({ Key key }) : super(key: key);
+
+  @override
+  _HomeScreenWidgetState createState() => _HomeScreenWidgetState();
+}
+
+class _HomeScreenWidgetState extends State<HomeScreenWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final _valueController = TextEditingController();
+
+  @override
+  void dispose() {
+    _valueController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
+        color: Colors.grey[800],
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15.0, 0.0, 0.0, 5.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: TextFormField(
+                  controller: _valueController,
+                  style: TextStyle(color: Colors.grey[300]),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search images...',
+                    contentPadding: EdgeInsets.fromLTRB(0, 17, 15, 0),
+                    hintStyle: TextStyle(
+                      color: Colors.grey[300]
+                    )
+                  ),
+                  validator: (value) {
+                    return value.isEmpty ? 'Please enter some text' : null;
+                  },
+                ),
+              ),
+              IconButton(
+                color: Colors.grey[300],
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    print(_valueController.text);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,68 +18,6 @@ class MyApp extends StatelessWidget {
           title: Text(_title)
         ),
         body: HomeScreenWidget()
-      ),
-    );
-  }
-}
-
-class HomeScreenWidget extends StatefulWidget {
-  HomeScreenWidget({ Key key }) : super(key: key);
-
-  @override
-  _HomeScreenWidgetState createState() => _HomeScreenWidgetState();
-}
-
-class _HomeScreenWidgetState extends State<HomeScreenWidget> {
-  final _formKey = GlobalKey<FormState>();
-  final _valueController = TextEditingController();
-
-  @override
-  void dispose() {
-    _valueController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Container(
-        color: Colors.grey[800],
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(15.0, 0.0, 0.0, 5.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: TextFormField(
-                  controller: _valueController,
-                  style: TextStyle(color: Colors.grey[300]),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Search images...',
-                    contentPadding: EdgeInsets.fromLTRB(0, 17, 15, 0),
-                    hintStyle: TextStyle(
-                      color: Colors.grey[300]
-                    )
-                  ),
-                  validator: (value) {
-                    return value.isEmpty ? 'Please enter some text' : null;
-                  },
-                ),
-              ),
-              IconButton(
-                color: Colors.grey[300],
-                icon: Icon(Icons.search),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    print(_valueController.text);
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
